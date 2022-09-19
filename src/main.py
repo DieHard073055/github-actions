4f2d696fc534846bdf6867f745adf3766149736b
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-09-19 17:04:14 +0800

    add subtraction

--- src/main.py
+++ src/main.py
@@ -11,9 +11,18 @@ def add(a: int, b: int) -> Result[int, Type[NumberTooSmallException]]:
     else:
         return Ok(a + b)
 
+def sub(a: int, b: int) -> Result[int, Type[NumberTooSmallException]]:
+    if a < 10 or b < 10:
+        return Err(NumberTooSmallException)
+    else:
+        return Ok(a - b)
+
+
 
 def main():
     result = add(10, 20)
+    print(result.unwrap_or(-1))
+    result = sub(10, 20)
     print(result.unwrap_or(-1))
 
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,9 @@
-from main import add
+from main import add, sub
 
 def test_add():
     result = add(10, 10)
     assert result.unwrap() == 20
+
+def test_sub():
+    result = sub(20, 10)
+    assert result.unwrap() == 10
